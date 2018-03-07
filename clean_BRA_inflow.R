029ef302455df02299fd5bcdb9bc665c3e5c3bec
Jackson M. Luckey <luckeyj@c097h171.acad.reed.edu>
2018-03-06 16:09:59 -0800

    drops unnecessary columns

--- clean_BRA_inflow.R
+++ clean_BRA_inflow.R
@@ -1,13 +1,15 @@
-library('dplyr')
-
+library(dplyr)
 library(readr)
-BRA_inflow <- read_csv("~/FDIcleanR/raw_data/FDIdata.xlsx - inflows (BRA).csv")
 
-# Rename columns I plan on using to provide structure
-BRA_inflow <- rename(BRA_inflow, Country = Brazil)
-BRA_inflow <- rename(BRA_inflow, '2001' = X7, '2002' = X8, '2003' = X9, '2004' = X10, '2005' = X11, '2006' = X12, '2007' = X13,
+# Import CSV
+BRA_inflow <- read_csv('~/FDIcleanR/raw_data/FDIdata.xlsx - inflows (BRA).csv')
+
+# Rename country and year columns
+BRA_inflow <- rename(BRA_inflow, Country = X5, '2001' = X7, '2002' = X8, '2003' = X9, '2004' = X10, '2005' = X11, '2006' = X12, '2007' = X13,
        '2008' = X14, '2009' = X15, '2010' = X16, '2011' = X17, '2012' = X18)
 
+# Drop unnecessary columns (do not do if regional FDI flows are required)
+BRA_inflow <- subset(BRA_inflow, select = -c(Brazil, X2, X3, X4, X6) )
 
 # Output cleaned dataset as CSV
 write.csv(BRA_inflow, '~/FDIcleanR/cleaned_data/BRA_inflow.csv')
