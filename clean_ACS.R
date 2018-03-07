cb2310cd8f78792da09ae1314ef19bbdb42f2efb
Jackson M. Luckey <luckeyj@c097h171.acad.reed.edu>
2018-03-07 15:14:39 -0800

    got removing useless rows working -> no idea why the column Statistics could not be called without renaming it, even if you renamed it to Statistics, even though RStudio's data viewer identified that as the column name

--- clean_ACS.R
+++ clean_ACS.R
@@ -1,6 +1,15 @@
 library(readr)
+library(dplyr)
 
 # Import raw data as CSV
 ACS <- read_csv('~/FDIcleanR/raw_data/acs5year.csv')
 
-ACS <- t(ACS)
+# Remove duplicate "rows" before transposing into columns
+names(ACS)[1] <- c('future_column')
+ACS <- filter(ACS, is.na(future_column) == FALSE )
+
+# Tranpose data so variables as columns, observations as rows
+# ACS <- t(ACS)
+
+# Write tidied dataframe to CSV
+write.csv(ACS, '~/FDIcleanR/cleaned_data/ACS.csv')
