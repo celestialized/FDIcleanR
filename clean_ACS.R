01821e5949aa4753d3e6197b353bbc2f8fd4110e
Jackson M. Luckey <luckeyj@c097h171.acad.reed.edu>
2018-03-07 15:53:10 -0800

    got the script to remove all nt_ACS rows with na in the first city column (checked to make sure that covered the same cases as testing all city columns before I got lazy

--- clean_ACS.R
+++ clean_ACS.R
@@ -5,7 +5,9 @@ nt_ACS <- read_csv('~/FDIcleanR/raw_data/acs5year.csv')
 
 # Removes worthless before transposing into columns
 names(nt_ACS)[1] <- c('future_column')
+names(nt_ACS)[2] <- c('Alameda_City')
 nt_ACS <- filter(nt_ACS, is.na(future_column) == FALSE )
+nt_ACS <- filter(nt_ACS, is.na(Alameda_City) == FALSE)
 
 # Tranpose data so variables as columns, observations as rows
 ACS <- t(nt_ACS)
